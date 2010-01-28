2fecfa63a40fb263e38afbbc3483c60f9d8f00fe
Tsung-Hsiang Chang <vgod@vgod.tw>
2010-01-28 12:51:34 -0500

    add .bat in Sikuli-IDE-win-*.zip release fixed Sikuli-IDE.bat for linking errors

--- sikuli-ide/target/win32/Sikuli-IDE.bat
+++ sikuli-ide/target/win32/Sikuli-IDE.bat
@@ -1,3 +1,3 @@
 @echo off
-PATH=%PATH%;"%~d0%~p0\tmplib"
+PATH=%PATH%;%~d0%~p0\tmplib
 java -Dpython.path="%~d0%~p0\sikuli-ide-full.jar\Lib" -cp "%~d0%~p0\sikuli-ide-full.jar" edu.mit.csail.uid.SikuliIDE %*
