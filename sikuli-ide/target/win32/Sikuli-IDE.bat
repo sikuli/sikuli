9c85a58553e946f5cca4f2ca603c1df03d56a263
Tsung-Hsiang Chang <vgod@vgod.tw>
2010-02-01 22:39:32 -0500

    fixed Bug #515406 - double "\" generated into Sikuli-IDE.bat add test_release in Makefile.win32

--- sikuli-ide/target/win32/Sikuli-IDE.bat
+++ sikuli-ide/target/win32/Sikuli-IDE.bat
@@ -1,3 +1,3 @@
 @echo off
-PATH=%PATH%;%~d0%~p0\tmplib
-java -Dpython.path="%~d0%~p0\sikuli-ide-full.jar\Lib" -cp "%~d0%~p0\sikuli-ide-full.jar" edu.mit.csail.uid.SikuliIDE %*
+PATH=%PATH%;%~d0%~p0tmplib
+java -Dpython.path="%~d0%~p0sikuli-ide-full.jar\Lib" -cp "%~d0%~p0sikuli-ide-full.jar" edu.mit.csail.uid.SikuliIDE %*
