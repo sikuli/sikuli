8a37a2e964cfc51497aa0504341722788c042ea9
Tsung-Hsiang Chang <vgod@vgod.tw>
2010-04-28 20:55:04 -0400

    win32 updated

--- lib/tmplib/change-lib-path.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-for dylib in *.dylib *.jnilib
-do
-   for ref in `otool -L $dylib | grep @executable | awk '{print $1'}`
-   do
-      install_name_tool -change $ref @loader_path/`basename $ref` $dylib
-   done
-done
