9e540bacaf94ca12e86794a860ea22ba42840595
Tsung-Hsiang Chang <vgod@vgod.tw>
2010-04-30 16:32:07 -0400

    update Sikuli-IDE.bat

--- sikuli-ide/target/win32/Sikuli-IDE.bat
+++ sikuli-ide/target/win32/Sikuli-IDE.bat
@@ -1,3 +1,5 @@
 @echo off
 PATH=%PATH%;%~d0%~p0tmplib
-java -Dfile.encoding=UTF-8 -Dpython.path="%~d0%~p0sikuli-ide-full.jar\Lib" -cp "%~d0%~p0sikuli-ide-full.jar" edu.mit.csail.uid.SikuliIDE %*
+java -Dfile.encoding=UTF-8 -Dpython.path="%~d0%~p0sikuli-script.jar/" -cp "%~d0%~p0jintellitype-1.3.2.jar;%~d0%~p0junit-3.8.1.jar;%~d0%~p0sikuli-ide.jar;%~d0%~p0sikuli-script.jar;%~d0%~p0swing-layout-1.0.1.jar" edu.mit.csail.uid.SikuliIDE %*
+
+
