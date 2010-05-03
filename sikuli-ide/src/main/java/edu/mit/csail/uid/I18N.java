e622edae53a09f8eb24ef853b34ceda9d283b851
Tsung-Hsiang Chang <vgod@vgod.tw>
2010-05-03 10:35:06 -0400

    fixed Bug #574249: use en_US interface if no locale files are found.

--- sikuli-ide/src/main/java/edu/mit/csail/uid/I18N.java
+++ sikuli-ide/src/main/java/edu/mit/csail/uid/I18N.java
@@ -12,21 +12,26 @@ public class I18N {
       Locale locale = Locale.getDefault();
       curLocale = locale;
       Debug.info("locale: " + locale);
-      i18nRB = ResourceBundle.getBundle("i18n/IDE",locale);
+      try{
+         i18nRB = ResourceBundle.getBundle("i18n/IDE",locale);
+      }
+      catch(MissingResourceException e){
+         Debug.info("no locale for " + locale);
+      }
       i18nRB_en = ResourceBundle.getBundle("i18n/IDE",locale_en);
    }
 
    static String _I(String key, Object... args){ 
       String ret;
-      try {
-         //ret = new String (i18nRB.getString(key).getBytes("ISO-8859-1"),"UTF-8");
-         ret = i18nRB.getString(key);
-      } catch (MissingResourceException e) {
+      if(i18nRB==null)
+         ret = i18nRB_en.getString(key); 
+      else{
+         try {
+            ret = i18nRB.getString(key);
+         } catch (MissingResourceException e) {
             ret = i18nRB_en.getString(key); 
-      } /*catch (UnsupportedEncodingException e) {
-         e.printStackTrace();
-         return null;
-      } */
+         } 
+      }
       if(args.length>0){
          MessageFormat formatter = new MessageFormat("");
          formatter.setLocale(curLocale);
