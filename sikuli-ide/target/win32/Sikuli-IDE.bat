bd8edad4ce6461c57de525e3e1d122ed6f9dbbb2
Tsung-Hsiang Chang <vgod@vgod.tw>
2010-05-21 12:55:04 -0400

    fix Sikuli-IDE.bat

--- sikuli-ide/target/win32/Sikuli-IDE.bat
+++ sikuli-ide/target/win32/Sikuli-IDE.bat
@@ -3,6 +3,6 @@ PATH=%PATH%;%~d0%~p0tmplib
 set JAVA_EXE="java"
 REM 64bit Windows users: please uncomment the following line and make sure the path is pointed to your 32bit JRE
 REM set JAVA_EXE="C:\Program Files (x86)\Java\jre6\bin\java.exe"
-%JAVA_EXE% -Xms64M -Xmx512M -Dfile.encoding=UTF-8 -Dpython.path="%~d0%~p0sikuli-script.jar/" -jar %~d0%~p0sikuli-ide.jar %*
+%JAVA_EXE% -Xms64M -Xmx512M -Dfile.encoding=UTF-8 -Dpython.path="%~d0%~p0sikuli-script.jar/" -jar "%~d0%~p0sikuli-ide.jar" %*
 
 
