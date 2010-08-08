e03692464e95b166092b9bd5455811a129a724a3
Tsung-Hsiang Chang <vgod@vgod.tw>
2010-08-07 22:14:09 -0700

    auto-detect x86 java in Sikuli-IDE.bat fixed unsatisfied linking error (Sikuli-IDE.exe) on 64bit Windows

--- sikuli-ide/target/win32/Sikuli-IDE.bat
+++ sikuli-ide/target/win32/Sikuli-IDE.bat
@@ -1,8 +1,9 @@
 @echo off
 PATH=%PATH%;%~d0%~p0tmplib
 set JAVA_EXE="java"
-REM 64bit Windows users: please uncomment the following line and make sure the path is pointed to your 32bit JRE
-REM set JAVA_EXE="C:\Program Files (x86)\Java\jre6\bin\java.exe"
+set JAVA_EXE_X86="C:\Program Files (x86)\Java\jre6\bin\java.exe"
+IF EXIST %JAVA_EXE_X86% SET JAVA_EXE=%JAVA_EXE_X86%
+
 %JAVA_EXE% -Xms64M -Xmx512M -Dfile.encoding=UTF-8 -Dpython.path="%~d0%~p0sikuli-script.jar/" -jar "%~d0%~p0sikuli-ide.jar" %*
 
 
