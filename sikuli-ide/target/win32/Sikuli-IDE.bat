76d9515c1fdc8e1320ab5e4a3418ac36ea1c65ae
Tsung-Hsiang Chang <vgod@vgod.tw>
2010-08-07 22:23:47 -0700

    fixed Bug #586699 use Brian Creation's patch for Sikuli-IDE.bat

--- sikuli-ide/target/win32/Sikuli-IDE.bat
+++ sikuli-ide/target/win32/Sikuli-IDE.bat
@@ -1,9 +1,6 @@
 @echo off
+
 PATH=%PATH%;%~d0%~p0tmplib
-set JAVA_EXE="java"
-set JAVA_EXE_X86="C:\Program Files (x86)\Java\jre6\bin\java.exe"
-IF EXIST %JAVA_EXE_X86% SET JAVA_EXE=%JAVA_EXE_X86%
-
-%JAVA_EXE% -Xms64M -Xmx512M -Dfile.encoding=UTF-8 -Dpython.path="%~d0%~p0sikuli-script.jar/" -jar "%~d0%~p0sikuli-ide.jar" %*
-
-
+set JAVA_EXE="javaw"
+if defined PROGRAMFILES(X86) set JAVA_EXE="%PROGRAMFILES(X86)%\Java\jre6\bin\javaw.exe"
+start /B "Silkuli-IDE Loader...." %JAVA_EXE% -Xms64M -Xmx512M -Dfile.encoding=UTF-8 -Dpython.path="%~d0%~p0sikuli-script.jar/" -jar "%~d0%~p0sikuli-ide.jar" %*
