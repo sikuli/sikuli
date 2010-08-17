cc471669de7bedcbca64a0b0cc29b4b7e8a6ad38
Tsung-Hsiang Chang <vgod@vgod.tw>
2010-08-16 21:11:12 -0700

    fixed win32's Sikuli-IDE.bat to run in the command line mode.

--- sikuli-ide/target/win32/Sikuli-IDE.bat
+++ sikuli-ide/target/win32/Sikuli-IDE.bat
@@ -1,6 +1,6 @@
 @echo off
 
 PATH=%PATH%;%~d0%~p0tmplib
-set JAVA_EXE="javaw"
-if defined PROGRAMFILES(X86) set JAVA_EXE="%PROGRAMFILES(X86)%\Java\jre6\bin\javaw.exe"
-start /B "Silkuli-IDE Loader...." %JAVA_EXE% -Xms64M -Xmx512M -Dfile.encoding=UTF-8 -Dpython.path="%~d0%~p0sikuli-script.jar/" -jar "%~d0%~p0sikuli-ide.jar" %*
+set JAVA_EXE="java"
+if defined PROGRAMFILES(X86) set JAVA_EXE="%PROGRAMFILES(X86)%\Java\jre6\bin\java.exe"
+%JAVA_EXE% -Xms64M -Xmx512M -Dfile.encoding=UTF-8 -Dpython.path="%~d0%~p0sikuli-script.jar/" -jar "%~d0%~p0sikuli-ide.jar" %*
