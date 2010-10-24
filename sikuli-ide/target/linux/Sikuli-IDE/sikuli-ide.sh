5d9ea159852b2b7815b5f5a4f90fa1ab1ae0a710
Tsung-Hsiang Chang <vgod@vgod.tw>
2010-10-23 23:54:55 -0400

    add the flag to enable/disable sikuli ide console window on Linux

--- sikuli-ide/target/linux/Sikuli-IDE/sikuli-ide.sh
+++ sikuli-ide/target/linux/Sikuli-IDE/sikuli-ide.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 DIR=`dirname $0`
-java -Dsikuli.console=false -Xms64M -Xmx512M -Dfile.encoding=UTF-8 -jar $DIR/sikuli-ide.jar $*
+java -Dsikuli.console=true -Xms64M -Xmx512M -Dfile.encoding=UTF-8 -jar $DIR/sikuli-ide.jar $*
