4f06e5ede15c21c850a8bb79ebb45f2c7136a3c0
Tsung-Hsiang Chang <vgod@vgod.tw>
2010-10-25 18:47:58 -0700

    add Win32Util back

---
+++ sikuli-script/src/main/native/Win32Util.cc
@@ -0,0 +1,91 @@
+#include "edu_mit_csail_uid_Win32Util.h"
+#include "windows.h"
+#include <cstring>
+#include <iostream>
+
+using namespace std;
+
+const int BUF_SIZE = 128;
+
+static bool strstr_i(char const *haystack, const char *pneedle){
+   int count = 0;
+   char buf[BUF_SIZE];
+   register char const *p;
+   char *needle, *q, *foundto;
+   if (!*pneedle) return true;
+   if (!haystack) return false;
+
+   needle = buf;
+   p = pneedle; q = needle;
+   
+   while ((*q++ = tolower(*p++)) && count<BUF_SIZE)
+      count++;
+   p = haystack - 1; foundto = needle;
+   while (*++p) {
+      if (tolower(*p) == *foundto) {
+         if (!*++foundto) {
+            return true;
+         }
+      } else foundto = needle;
+   }
+   return false;
+}
+
+
+static const char *gAppName = NULL;
+static BOOL CALLBACK killWindow(HWND handle, long lParam){
+   char buf[BUF_SIZE];
+   GetWindowText(handle, buf, BUF_SIZE);
+   if( strstr_i(buf, gAppName) != NULL ){
+      DWORD pid;
+      GetWindowThreadProcessId(handle, &pid);
+      HANDLE proc = OpenProcess(SYNCHRONIZE|PROCESS_TERMINATE, FALSE, pid);
+      TerminateProcess(proc, 0);
+      CloseHandle(proc);
+      return FALSE;
+   }
+   return TRUE;
+}
+
+static BOOL CALLBACK findWindow(HWND handle, long lParam){
+   char buf[BUF_SIZE];
+   GetWindowText(handle, buf, BUF_SIZE);
+   if( strstr_i(buf, gAppName) != NULL ){
+      SetForegroundWindow(handle);
+      return FALSE;
+   }
+   return TRUE;
+}
+
+JNIEXPORT jint JNICALL Java_edu_mit_csail_uid_Win32Util_switchApp(JNIEnv *env, jobject jobj, jstring jAppName){
+
+   gAppName = env->GetStringUTFChars(jAppName, NULL);
+   BOOL result = EnumWindows((WNDENUMPROC)findWindow, 0);
+   env->ReleaseStringUTFChars(jAppName, gAppName);
+   if( result != 0){ // switch failed. open it
+		return Java_edu_mit_csail_uid_Win32Util_openApp(env, jobj, jAppName);
+   }
+   return result;
+}
+
+JNIEXPORT jint JNICALL Java_edu_mit_csail_uid_Win32Util_openApp(JNIEnv *env, jobject jobj, jstring jAppName){
+   const char *appName = env->GetStringUTFChars(jAppName, NULL);
+   int n = strlen(appName);
+   char *buf = new char[n+3];
+   strncpy(buf+1, appName, n);
+   buf[0] = '"';
+   buf[n+1] = '"';
+   buf[n+2] = 0;
+   int result = WinExec(buf, SW_SHOWNORMAL);
+   delete [] buf;
+   env->ReleaseStringUTFChars(jAppName, appName);
+   if( result > 31 ) return 0;
+   return -1;
+}
+
+JNIEXPORT jint JNICALL Java_edu_mit_csail_uid_Win32Util_closeApp(JNIEnv *env, jobject jobj, jstring jAppName){
+   gAppName = env->GetStringUTFChars(jAppName, NULL);
+   BOOL result = EnumWindows((WNDENUMPROC)killWindow, 0);
+   env->ReleaseStringUTFChars(jAppName, gAppName);
+   return result;
+}
