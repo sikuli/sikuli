ff568d3c573b53385b864a5df6327efc8fa85cca
Tsung-Hsiang Chang <vgod@vgod.tw>
2010-10-28 14:59:59 -0400

    fixed the env variable setting for tesseract

--- sikuli-script/src/main/native/tessocr.cpp
+++ sikuli-script/src/main/native/tessocr.cpp
@@ -329,7 +329,7 @@ OCR::init(const char* datapath){
    char outputbase[] = "output";
    char lang[] = "eng";
    bool numeric_mode = false;
-   string env_datapath = string("TESSDATA_PREFIX") + string(datapath);
+   string env_datapath = string("TESSDATA_PREFIX=") + string(datapath);
    putenv(const_cast<char*>(env_datapath.c_str()));
    int ret = TessBaseAPI::InitWithLanguage(datapath,outputbase,lang,NULL,numeric_mode,0,0);
    cout << (ret==0?"done":"failed") << endl;
