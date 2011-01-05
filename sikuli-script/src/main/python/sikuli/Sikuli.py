96a7656d75435c1a392b3d6d5a61bab2c9d51adf
Tsung-Hsiang Chang <vgod@vgod.tw>
2011-01-04 19:16:53 -0500

    load() searches for the jar file in sikuli data path and then in bundle path

--- sikuli-script/src/main/python/sikuli/Sikuli.py
+++ sikuli-script/src/main/python/sikuli/Sikuli.py
@@ -34,9 +34,32 @@ import SikuliImporter
 
 _si = SikuliScript()
 
+##
+# loads a Sikuli extension (.jar) from
+#  1. user's sikuli data path
+#  2. bundle path 
+#
 def load(jar):
-   if not jar in sys.path:
-      sys.path.append(jar)
+   import os
+   from org.sikuli.script import ExtensionManager
+   def _load(abspath):
+      if os.path.exists(abspath):
+         if not abspath in sys.path:
+            sys.path.append(abspath)
+            return True
+      return False
+
+   if _load(jar):
+      return True
+   path = ExtensionManager.getInstance().getUserExtPath()
+   jarInExtPath = path + java.io.File.separator + jar
+   if _load(jarInExtPath):
+      return True
+   path = getBundlePath()
+   jarInBundle = path + java.io.File.separator + jar
+   if _load(jarInBundle):
+      return True
+   return False
 
 def addImagePath(path):
    ImageLocator.addImagePath(path)
