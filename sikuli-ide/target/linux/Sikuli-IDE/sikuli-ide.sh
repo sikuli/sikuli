447e557ca68a2a9c717baf5ceddcf033167ad850
Tsung-Hsiang Chang <vgod@vgod.tw>
2011-02-10 11:34:29 -0500

    add LC_NUMERIC=C to linux's sikuli-ide.sh

--- sikuli-ide/target/linux/Sikuli-IDE/sikuli-ide.sh
+++ sikuli-ide/target/linux/Sikuli-IDE/sikuli-ide.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 DIR=`dirname $0`
-java -Dsikuli.console=true -Dsikuli.debug=0 -Xms64M -Xmx512M -Dfile.encoding=UTF-8 -jar $DIR/sikuli-ide.jar $*
+LC_NUMERIC=C java -Dsikuli.console=true -Dsikuli.debug=0 -Xms64M -Xmx512M -Dfile.encoding=UTF-8 -jar $DIR/sikuli-ide.jar $*
