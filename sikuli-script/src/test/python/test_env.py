b8645c734e43033f7355555494be69ca5114f672
Tsung-Hsiang Chang <vgod@vgod.tw>
2011-05-23 14:29:17 -0400

    migrate old test_env.py

---
+++ sikuli-script/src/test/python/test_env.py
@@ -0,0 +1,36 @@
+# Copyright 2010-2011, Sikuli.org
+# Released under the MIT License.
+import unittest
+from sikuli import *
+import os
+import random
+from java.lang import System
+
+class TestEnv(unittest.TestCase):
+   def testOS(self):
+      if Env.getOS() == OS.MAC:
+         assert System.getProperty("os.name").startswith("Mac OS X")
+      elif Env.getOS() == OS.WINDOWS:
+         assert System.getProperty("os.name").startswith("Windows")
+      elif Env.getOS() == OS.LINUX:
+         assert System.getProperty("os.name").find("Linux") >= 0
+      assert Env.getOSVersion() != None
+
+   def testCapsLock(self):
+      lock = Env.isLockOn(Key.CAPS_LOCK)
+      assert (lock == True or lock == False)
+
+   def testSikuliVersion(self):
+      ver = Env.getSikuliVersion()
+      assert ver != None
+
+   def testMouseLocation(self):
+      b = getBounds()
+      Settings.MoveMouseDelay = 0
+      for i in range(0,10):
+         x = random.randint(b.getX(),b.getWidth())
+         y = random.randint(b.getY(),b.getHeight())
+         hover(Location(x,y))
+         cursor = Env.getMouseLocation()
+         assert cursor.x == x and cursor.y == y
+
