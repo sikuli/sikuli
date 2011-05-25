74122dd0631eddf7f2a564a2806e6dd6b67958ca
Tsung-Hsiang Chang <vgod@vgod.tw>
2011-05-25 17:43:26 -0400

    change-lib-path.sh: change dependencies of all libs that are not in /usr/lib and /System

--- sikuli-ide/target/osx-10.6/Sikuli-IDE.app/Contents/Frameworks/change-lib-path.sh
+++ sikuli-ide/target/osx-10.6/Sikuli-IDE.app/Contents/Frameworks/change-lib-path.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 for dylib in *.dylib *.jnilib
 do
-   for ref in `otool -L $dylib | grep "lib.*.dylib[^:]" | awk '{print $1'} | grep ^[^/]`
+   for ref in `otool -L $dylib | grep "lib.*.dylib[^:]" | awk '{print $1'} | grep -v '^/usr/lib' | grep -v '^/System/'`
    do
       #echo $ref
       install_name_tool -change $ref @loader_path/`basename $ref` $dylib
