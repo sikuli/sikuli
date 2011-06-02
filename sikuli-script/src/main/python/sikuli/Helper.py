9642c1dac0d2a39db9cd74ff2d2f64f87c618a26
Tsung-Hsiang Chang <vgod@vgod.tw>
2011-06-02 11:42:03 -0400

    add two Helper functions for sorting matches: byX and byY

--- sikuli-script/src/main/python/sikuli/Helper.py
+++ sikuli-script/src/main/python/sikuli/Helper.py
@@ -4,3 +4,9 @@ from math import sqrt
 
 def byDistanceTo(m):
    return lambda a,b: sqrt((a.x-m.x)**2+(a.y-m.y)**2) - sqrt((b.x-m.x)**2+(b.y-m.y)**2)
+
+def byX(m):
+   return m.x
+
+def byY(m):
+   return m.y
