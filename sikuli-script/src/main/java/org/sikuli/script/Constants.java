78df2ae8db8808d05043e62df02c8c57ff4722da
Tsung-Hsiang Chang <vgod@vgod.tw>
2011-06-28 18:49:51 -0400

    add Constants.java

---
+++ sikuli-script/src/main/java/org/sikuli/script/Constants.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2010-2011, Sikuli.org
+ * Released under the MIT License.
+ *
+ */
+package org.sikuli.script;
+
+public class Constants {
+   public static float FOREVER = Float.POSITIVE_INFINITY;
+}
+
