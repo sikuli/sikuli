ef91d9e211dd3049afc831b606145023c91613de
Tsung-Hsiang Chang <vgod@vgod.tw>
2011-08-20 19:10:32 -0400

    add LinuxHotkeyManager

--- sikuli-ide/src/main/java/org/sikuli/ide/NativeLayerForLinux.java
+++ sikuli-ide/src/main/java/org/sikuli/ide/NativeLayerForLinux.java
@@ -5,89 +5,12 @@
  */
 package org.sikuli.ide;
 
-import java.lang.reflect.*;
-import java.awt.Event;
-import java.awt.event.*;
-import java.util.*;
-import java.io.*;
-import com.wapmx.nativeutils.jniloader.NativeLoader;
-import jxgrabkey.HotkeyConflictException;
-import jxgrabkey.HotkeyListener;
-import jxgrabkey.JXGrabKey;
-
-
-import org.sikuli.script.Debug;
-
 public class NativeLayerForLinux implements NativeLayer {
-   private Map<String, Integer> _callbackIdMap = new HashMap<String,Integer>();
-   private Map<Integer, String> _idCallbackMap = new HashMap<Integer,String>();
-
    public void initApp(){}
 
    public void initIDE(SikuliIDE ide){
-      try{
-         NativeLoader.loadLibrary("JXGrabKey");      
-      }
-      catch(IOException e){
-         Debug.error("Can't load native lib JXGrabKey");
-         e.printStackTrace();
-      }
    }
 
-   public void installHotkey(int key, int mod, 
-                              final SikuliIDE ide, 
-			      final String callbackMethod, String callbackType){ 
-
-      String txtMod = KeyEvent.getKeyModifiersText(mod).toUpperCase();
-      txtMod = txtMod.replace("META","WIN");
-      txtMod = txtMod.replace("WINDOWS","WIN");
-      String txtCode = KeyEvent.getKeyText(key).toUpperCase();
-      Debug.info("install hotkey: " + txtMod + "+" + txtCode + 
-                   " for " + callbackMethod);
-
-      JXGrabKey grabKey = JXGrabKey.getInstance();
-      int id;
-      if( _callbackIdMap.containsKey(callbackMethod) ){
-         id = _callbackIdMap.get(callbackMethod);
-         grabKey.unregisterHotKey(id); 
-      }
-      else{
-         id = _callbackIdMap.size()+1;
-         _callbackIdMap.put(callbackMethod, id);
-         _idCallbackMap.put(id, callbackMethod);
-      }
-      try{
-         //JXGrabKey.setDebugOutput(true);
-         grabKey.registerAwtHotkey(id, mod, key);
-      }catch(HotkeyConflictException e){
-         Debug.error("Hot key conflicts");
-         grabKey.cleanUp(); 
-         return;
-      }
-				
-		//Implement HotkeyListener
-
-      if(_callbackIdMap.size()==1){
-         HotkeyListener hotkeyListener = new jxgrabkey.HotkeyListener(){
-            public void onHotkey(int id) {
-               Debug.log(2, "onHotkey: " + id);
-               String callbackFunc = _idCallbackMap.get(id);
-               Class params[] = {};
-               Object paramsObj[] = {};
-               Class cls = ide.getClass();
-               try{
-                  Method callback = cls.getDeclaredMethod(callbackFunc, params);
-                  callback.invoke(ide, paramsObj);
-               }
-               catch(Exception e){
-                  e.printStackTrace();
-               }
-            }
-         };
-         JXGrabKey.getInstance().addHotkeyListener(hotkeyListener);
-      }
-		
-   }
 }
 
 
---
+++ sikuli-script/src/main/java/org/sikuli/script/internal/hotkey/LinuxHotkeyManager.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2010-2011, Sikuli.org
+ * Released under the MIT License.
+ *
+ */
+package org.sikuli.script.internal.hotkey;
+
+import java.lang.reflect.*;
+import java.awt.Event;
+import java.awt.event.*;
+import java.util.*;
+import java.io.IOException;
+
+import org.sikuli.script.Debug;
+import org.sikuli.script.HotkeyListener;
+import org.sikuli.script.HotkeyEvent;
+
+import com.wapmx.nativeutils.jniloader.NativeLoader;
+import jxgrabkey.HotkeyConflictException;
+import jxgrabkey.JXGrabKey;
+
+public class LinuxHotkeyManager extends HotkeyManager {
+   static{
+      try{
+         NativeLoader.loadLibrary("JXGrabKey");
+      }
+      catch(IOException e){
+         Debug.error("Can't load native lib JXGrabKey");
+         e.printStackTrace();
+      }
+   }
+
+   class HotkeyData {
+      int key, modifiers;
+      HotkeyListener listener;
+
+      public HotkeyData(int key_, int mod_, HotkeyListener l_){
+         key = key_;
+         modifiers = mod_;
+         listener = l_;
+      }
+   };
+
+   class MyHotkeyHandler implements jxgrabkey.HotkeyListener{
+      public void onHotkey(int id){
+         Debug.log(2, "Hotkey pressed");
+         HotkeyData data = _idCallbackMap.get(id);
+         HotkeyEvent e = new HotkeyEvent(data.key, data.modifiers);
+         data.listener.hotkeyPressed(e);
+      }
+   };
+
+   private Map<Integer, HotkeyData> _idCallbackMap = new HashMap<Integer,HotkeyData >();
+   private int _gHotkeyId = 1;
+
+   public boolean addHotkey(int keyCode, int modifiers, HotkeyListener listener){
+      JXGrabKey grabKey = JXGrabKey.getInstance();
+      String txtMod = KeyEvent.getKeyModifiersText(modifiers).toUpperCase();
+      txtMod = txtMod.replace("META","WIN");
+      txtMod = txtMod.replace("WINDOWS","WIN");
+      String txtCode = KeyEvent.getKeyText(keyCode).toUpperCase();
+      Debug.info("install hotkey: " + txtMod + "+" + txtCode);
+
+      if(_gHotkeyId == 1){
+         grabKey.addHotkeyListener(new MyHotkeyHandler());
+      }
+
+      removeHotkey(keyCode, modifiers);
+      int id = _gHotkeyId++;
+      HotkeyData data = new HotkeyData(keyCode, modifiers, listener);
+      _idCallbackMap.put(id, data);
+
+      try{
+         //JXGrabKey.setDebugOutput(true);
+         grabKey.registerAwtHotkey(id, modifiers, keyCode);
+      }catch(HotkeyConflictException e){
+         Debug.error("Hot key conflicts: " + txtMod + "+" + txtCode);
+         return false;
+      }
+      return true;
+   }
+
+   public boolean removeHotkey(int keyCode, int modifiers){
+      for( Map.Entry<Integer, HotkeyData> entry : _idCallbackMap.entrySet() ){
+         HotkeyData data = entry.getValue();
+         if(data.key == keyCode && data.modifiers == modifiers){
+            JXGrabKey grabKey = JXGrabKey.getInstance();
+            int id = entry.getKey();
+            grabKey.unregisterHotKey(id); 
+            _idCallbackMap.remove(id);
+            return true;
+         }
+      }
+      return false;
+   }
+
+   public void cleanUp(){
+      JXGrabKey.getInstance().cleanUp(); 
+   }
+
+}
+
+
