2fff71fd006cf94a7f71e279f1285baeb3e1b285
Tsung-Hsiang Chang <vgod@vgod.tw>
2011-08-20 12:50:32 -0700

    add WindowsHotkeyManager

--- sikuli-script/src/main/java/org/sikuli/script/HotkeyEvent.java
+++ sikuli-script/src/main/java/org/sikuli/script/HotkeyEvent.java
@@ -10,6 +10,10 @@ public class HotkeyEvent {
    public int keyCode;
    public int modifiers;
 
+   public HotkeyEvent(int code_, int mod_){
+      init(code_, mod_);
+   }
+
    void init(int code_, int mod_){
       keyCode = code_;
       modifiers = mod_;
