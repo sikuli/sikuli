61675d4d2ef1c63e9f9332380a62b895ef9ec1b7
Tsung-Hsiang Chang <vgod@vgod.tw>
2011-09-06 15:26:35 -0400

    add an android example script

---
+++ extensions/android-robot/example/android-example.py
@@ -0,0 +1,45 @@
+import unittest
+import os
+from org.sikuli.script.android import AndroidScreen
+from org.sikuli.script import Settings
+
+print "set up android connection"
+scr = AndroidScreen() # compatiable with Sikuli Screen/Region
+assert scr != None
+dev = scr.getRobot().getDevice() # Android Monkey device
+
+Settings.BundlePath = os.getcwd() + "/test-res/"
+Settings.MoveMouseDelay = 0
+Settings.AutoWaitTimeout = 10
+
+
+class TestAndroidBasic(unittest.TestCase):
+
+   def testBounds(self):
+      global scr, dev
+      bounds = scr.getBounds()
+      assert(bounds.x == 0)
+      assert(bounds.y == 0)
+      assert(bounds.width > 0)
+      assert(bounds.height > 0)
+
+   def testCapture(self):
+      global scr, dev
+      img = scr.capture()
+      assert img != None
+
+   def testDrag(self):
+      global scr, dev
+      if not scr.exists("lock.png"):
+         dev.press('KEYCODE_POWER', 'DOWN_AND_UP')
+      scr.dragDrop("lock.png", "speaker.png")
+
+   def testType(self):
+      global scr, dev
+      dev.press('KEYCODE_HOME', 'DOWN_AND_UP')
+      scr.click("google.png")
+      scr.type("sikuli")
+      assert scr.exists("search-sikuli.png")
+
+if __name__ == '__main__':
+   unittest.main()
--- extensions/android-robot/run-sikuli-monkey.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-SIKULI_SCRIPT_JAR=sikuli-script.jar
-ANDROID_SDK=/opt/android-sdk-mac_x86
-java -cp $SIKULI_SCRIPT_JAR:$ANDROID_SDK/tools/lib/monkeyrunner.jar -Djava.ext.dirs=$ANDROID_SDK/tools/lib -Djava.library.path=$ANDROID_SDK/tools/lib -Dcom.android.monkeyrunner.bindir=$ANDROID_SDK/tools -Dpython.path=$SIKULI_SCRIPT_JAR com.android.monkeyrunner.MonkeyRunnerStarter $*
