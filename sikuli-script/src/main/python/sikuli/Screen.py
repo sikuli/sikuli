944edfc5edefd574bb90bcf627985ecc4b25ffe8
Liam Deacon <liam.m.deacon@gmail.com>
2017-01-13 16:37:44 +0000

    Improved Python Screen wrapper and added tests
    Added some magic methods to provide a more pythonic interface as well as
    adding a few tests to probe their behaviour.

--- sikuli-script/src/main/python/sikuli/Screen.py
+++ sikuli-script/src/main/python/sikuli/Screen.py
@@ -12,6 +12,7 @@ from java.awt import Rectangle
 DEBUG=False
 
 class Screen(Region):
+
    def __init__(self, id=None):
       if id != None:
          r = JScreen.getBounds(id)
@@ -20,7 +21,68 @@ class Screen(Region):
       (x, y, w, h) = (int(r.getX()), int(r.getY()), \
                       int(r.getWidth()), int(r.getHeight()))
       Region.__init__(self, x, y, w, h)
+      self.id = int(id) if id else 0
 
+   def __int__(self):
+       return self.id
+
+   # iterates over screens - may be useful to search for something in each screen
+   def __iter__(self):
+      return iter(self.__class__(i) for i in range(len(self)))
+   
+   # returns whether screen has a valid id number
+   def __bool__(self):
+       return self.id >= 0 and self.id < len(self)
+    
+   # shorthand for getting the number of screens
+   def __len__(self):
+       return JScreen.getNumberScreens() 
+   
+   def __contains__(self, other):
+       ''' Checks whether other is (completely) within the given screen 
+       
+       Arguments:
+           other (Tuple|List|Region|Location) - the item to check
+       
+       Returns:
+           bool
+       '''
+       from collections import Iterable
+       x, y = getattr(other, 'x', 0), getattr(other, 'y', 0)
+       w, h = getattr(other, 'w', 0), getattr(other, 'h', 0)
+
+       if isinstance(other, Iterable) and not isinstance(self.__class__):
+           if len(other) == 2:
+              x, y = other
+           elif len(other) == 4:
+              x, y, w, h = other
+       elif isinstance(other, Rectangle):
+           x, y, w, h = (other.getX(), other.getY(), 
+                         other.getWidth(), other.getHeight())
+       within = (x >= self.x and x+w <= self.x+self.w and 
+                 y >= self.y and y+h <= self.y+self.h)
+
+       screen = other.getScreen() if (getattr(other, 'getScreen', None) and 
+                                      callable(other.getScreen)) else None
+       if not screen:
+           return within
+       else:
+           # cheat for screen compare as cannot guarantee int/hash methods work
+           # this is probably not needed as Regions map to screens based on coordinates 
+           return within and repr(screen) == repr(self) 
+   
+   def __hash__(self):
+       return self.id
+   
+   def __cmp__(self, other):
+       return int(self) - int(other)
+    
+   def __eq__(self, other):
+       return hash(self) == hash(other)
+
+   def __ne__(self, other):
+       return not self.__eq__(other)
+            
    @classmethod
    def getNumberScreens(cls):
       return JScreen.getNumberScreens()
@@ -40,7 +102,6 @@ class Screen(Region):
 
    def showRegion(self, region):
       self.getScreen().showRegion(region)
-
 
    ##
    # Enters the screen-capture mode asking the user to capture a region of 
@@ -72,7 +133,6 @@ class Screen(Region):
       else:
          return None
 
-
    def toString(self):
       return self.getScreen().toString()
 
@@ -89,4 +149,4 @@ class Screen(Region):
             if DEBUG: print "expose " + name
             dict[name] = eval("self."+name)
             #__main__.__dict__[name] = eval("self."+name)
-
+  
